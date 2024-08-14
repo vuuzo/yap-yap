0bf729d634ad0d31a28cd39b1750958f75cd46ee
vuuzo <kaplann.oskar@gmail.com>
2024-08-14 23:16:00 +0200

    add custom img wrapper

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,12 +1,21 @@
 import * as runtime from "react/jsx-runtime";
 import GrowElement from "./grow-element";
+import { ComponentProps } from "react";
 
 const useMDXComponent = (code: string) => {
   const fn = new Function(code);
   return fn({ ...runtime }).default;
 };
 
-const components = { GrowElement };
+const components = {
+  GrowElement,
+  img: (props: ComponentProps<"img">) => (
+    <span className='relative block z-10 rounded-xl overflow-hidden my-4'>
+      <img className='not-prose' {...props} />
+      <span className='absolute inset-0 shadow-inset-border rounded-xl' />
+    </span>
+  ),
+};
 
 interface MDXProps {
   code: string;
