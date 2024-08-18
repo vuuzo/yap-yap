c204459b8c086323e7a0a1ffbe6fcf0b912b4921
vuuzo <kaplann.oskar@gmail.com>
2024-08-18 18:06:42 +0200

    fix: colors

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -8,23 +8,18 @@ export function PageHeader({
   className?: string;
 }) {
   return (
-    <header
-      className={cn(
-        "flex items-center justify-center flex-col min-h-52",
-        className
-      )}
-    >
-      {children}
-    </header>
+    <header className={cn("min-h-36 py-10", className)}>{children}</header>
   );
 }
 
 export function Heading({ children }: { children: React.ReactNode }) {
   return (
-    <h1 className='text-4xl dark:text-zinc-200 font-bold mb-2'>{children}</h1>
+    <h1 className='text-3xl text-zinc-900 dark:text-zinc-100 font-medium mb-2'>
+      {children}
+    </h1>
   );
 }
 
 export function Subheading({ children }: { children: React.ReactNode }) {
-  return <h3 className='text-zinc-600 dark:text-zinc-400'>{children}</h3>;
+  return <h3 className='text-zinc-700 dark:text-zinc-300'>{children}</h3>;
 }
