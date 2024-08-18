072c6231c9950dea40b41e19f607ca504ca8da52
vuuzo <kaplann.oskar@gmail.com>
2024-08-18 18:07:03 +0200

    feat: add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-import { PageHeader, Heading } from "@/components/page-header";
+import { posts } from "#site/content";
+import { PageHeader, Heading, Subheading } from "@/components/page-header";
+import PostItem from "@/components/post-item";
+import { sortPosts } from "@/lib/utils";
 
 export default function Home() {
+  const sortedPosts = sortPosts(posts.filter((post) => post.published));
+  const displayPosts = sortedPosts.slice(0, 4);
   return (
-    <main className='col-span-1 col-start-3'>
-      <PageHeader>
-        <Heading>{`{homepage}`}</Heading>
+    <main>
+      <PageHeader className='mb-20 flex-col items-start gap-5'>
+        <Heading>Yap Yap</Heading>
+        <Subheading>
+          Hey You! This is a good place to introduce yourself.
+        </Subheading>
       </PageHeader>
+      <section>
+        <h2 className='mb-5'>Latest posts</h2>
+        <ul>
+          {displayPosts.map((post) => (
+            <li key={post.slugAsParams} className='mb-2'>
+              <PostItem post={post} />
+            </li>
+          ))}
+        </ul>
+      </section>
     </main>
   );
 }
